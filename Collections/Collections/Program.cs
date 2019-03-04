6fd2ff19244c54c544df74dc77cd52b2fb209e73
HelenSolovey <47430048+HelenSolovey@users.noreply.github.com>
2019-03-05 00:57:07 +0300

    Add files via upload

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Collections
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList test = Number(3, 5);
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+        }
+
+        public static ArrayList Number(double a, double q)
+        {
+          
+            ArrayList numberList = new ArrayList();
+
+            for(int i = 0;  i< 20; i++)
+            {
+                a += q ;
+                numberList.Add(a);
+            }
+            return numberList;
+        }
+
+    }
+}
